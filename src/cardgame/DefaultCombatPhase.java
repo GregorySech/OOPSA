3b1291cd4579240ef0f71567feb5f86a461b0f44
gsech <gsech@new-host.station>
2017-04-13 19:15:41 +0200

    Aggiunte firme (+commenti) delle sottofasi della combat.

--- src/cardgame/DefaultCombatPhase.java
+++ src/cardgame/DefaultCombatPhase.java
@@ -5,11 +5,41 @@
  */
 package cardgame;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author atorsell
  */
 public class DefaultCombatPhase implements Phase {
+    /**
+     * Asks the CurrentPlayer which creature will start the attack. 
+     * Next there is a stack fill started by the enemy of CurrentPlayer.
+     * @return list of attackers chosen by the currentPlayer. 
+     */
+    protected List<Creature> attackSubPhase(){
+        return null;
+    }
+    
+    /**
+     * Asks the enemy of CurrentPlayer if and how the attackers will be blocked.
+     * Next there is a stack fill started by CurrentPlayer.
+     * @param attackers list of attackers.
+     * @return Map(attacker, defenders) chosen by the enemy of CurrentPlayer.
+     */
+    protected Map<Creature, List<Creature>> defenceSubPhase(List<Creature> attackers){
+        return null;
+    }
+    /**
+     * 
+     * @param battles Map(attacker, defenders) each entry is a single battle. 
+     * a battle with no defenders will be creature vs player.
+     */
+    protected void damageSubPhase(Map<Creature, List<Creature>> battles){
+        
+    }
+    
     @Override
     public void execute() {
         Player currentPlayer = CardGame.instance.getCurrentPlayer();
