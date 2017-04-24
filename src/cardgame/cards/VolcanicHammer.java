ed8b50db128a32dd9affc13fac5bd7fb9e54c904
Hp <Hp@Jona>
2017-04-24 12:10:05 +0200

    VolcanicHammer2

--- src/cardgame/cards/VolcanicHammer.java
+++ src/cardgame/cards/VolcanicHammer.java
@@ -12,6 +12,7 @@ import cardgame.Creature;
 import cardgame.Effect;
 import cardgame.Player;
 import cardgame.SingleTargetEffect;
+import java.util.List;
 
 /**
  *
@@ -40,17 +41,59 @@ public class VolcanicHammer implements Card {
          }
 
         @Override
+        @SuppressWarnings("empty-statement")
         public void chooseTarget() {
             int last;
             System.out.println("Volcanic Hammer targetting phase : ");
             do{
-                System.out.println("Choose the target :");
-                System.out.println("[1]" + CardGame.instance.getCurrentPlayer().name());
-                System.out.println("[2]" + CardGame.instance.getCurrentAdversary().name());
-                System.out.println("[3]" + CardGame.instance.getCurrentPlayer().getCreatures());
-                System.out.println("[4]" + CardGame.instance.getCurrentAdversary().getCreatures());
+                System.out.println("What do you want to target :");
+                System.out.println("[1]" + "A Player");
+                System.out.println("[2]" + " A Creature");
                 last= CardGame.instance.getScanner().nextInt();
+                if(last==1){
+                    do{
+                        System.out.println("[1]" + CardGame.instance.getCurrentPlayer().name());
+                        System.out.println("[2]" + CardGame.instance.getCurrentAdversary().name());
+                        last= CardGame.instance.getScanner().nextInt();
+                    }while(last<1 || last >2);
+                    if(last==1)
+                       target=CardGame.instance.getCurrentPlayer();
+                    else 
+                       target=CardGame.instance.getCurrentAdversary();
+                }
+                else {
+                    System.out.println("What creature do you want to target:");
+                     do{
+                        System.out.println("[1]" + "Player creatures");
+                        System.out.println("[2]" +"Adversary Creature");
+                        last= CardGame.instance.getScanner().nextInt();
+                    }while(last<1 || last >2);
+                    if(last==1){
+                        List <Creature> playercreature=CardGame.instance.getCurrentPlayer().getCreatures();
+                        for(Creature c: playercreature){
+                            System.out.println(playercreature.get(last));
+                            last= CardGame.instance.getScanner().nextInt();
+                        }
+                        if(! playercreature.isEmpty()){
+                            target=playercreature.get(last);
+                        }
+                       
+                    }
+                    else{
+                        List <Creature> adversarycreature=CardGame.instance.getCurrentAdversary().getCreatures();
+                        for(Creature c: adversarycreature){
+                            System.out.println(adversarycreature.get(last));
+                            last= CardGame.instance.getScanner().nextInt();
+                        }
+                        if(! adversarycreature.isEmpty()){
+                            target=adversarycreature.get(last);
+                        }
+                    }
+                                              
+                }
+                
             }while(last < 1 || last > 2);
+            
         }
 
         @Override
