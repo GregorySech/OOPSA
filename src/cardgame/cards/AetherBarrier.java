41e22d5b588ff0696fd2e5d3fb558560859c0a9e
jonais <>
2017-04-27 21:55:30 +0200

    AetherBarrier (da testare)

--- src/cardgame/cards/AetherBarrier.java
+++ src/cardgame/cards/AetherBarrier.java
@@ -8,9 +8,16 @@ package cardgame.cards;
 import cardgame.AbstractEnchantment;
 import cardgame.AbstractEnchantmentCardEffect;
 import cardgame.Card;
+import cardgame.CardGame;
+import cardgame.Creature;
 import cardgame.Effect;
 import cardgame.Enchantment;
+import cardgame.Permanent;
 import cardgame.Player;
+import cardgame.TriggerAction;
+import static cardgame.Triggers.EFFECT_CASTED;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -18,7 +25,7 @@ import cardgame.Player;
  */
 public class AetherBarrier implements Card {
     
-    private class AetherBarrierEffect extends AbstractEnchantmentCardEffect{
+    private class AetherBarrierEffect extends AbstractEnchantmentCardEffect {
         
         AetherBarrierEffect(Player p, Card c){
             super(p, c);
@@ -28,19 +35,85 @@ public class AetherBarrier implements Card {
         protected Enchantment createEnchantment() {
            return new AetherBarrierEnchantment(owner);
         }
-        
+         
     }
     
-    private class AetherBarrierEnchantment extends AbstractEnchantment{
+    private class AetherBarrierEnchantment extends AbstractEnchantment  {
+       private Permanent target;
+       TriggerAction D;
         AetherBarrierEnchantment(Player owner){
             super(owner);
         }
+        
+        
+       private void choosePermanent(Player p) {
+            int i = 0, j;
+            List<Permanent> plP = new ArrayList();
+            List<Creature> c= p.getCreatures();
+            List<Enchantment> e= p.getEnchantments();
+
+            for (Creature ci : c) {
+                plP.add(ci);
+            }
+            for (Enchantment ei : e) {
+                plP.add(ei);
+            }
+            
+            do {
+                i = 0;
+                for (Permanent pe : plP) {
+                    System.out.println("[" + (++i) + "]" + c);
+                }
+                System.out.println("[0] to end selection");
+                j = CardGame.instance.getScanner().nextInt();
+                if (j != 0 && j <= plP.size()) {
+                    target = plP.get(j - 1);
+                } else {
+                    target = null;
+                }
+            } while (j < 0 || j > plP.size());
+        }
+
+      
+        
+        private class AetherBarrierTrigger implements TriggerAction{
+        
+            Player p;
+            
+            private AetherBarrierTrigger (Player owner){
+                p= owner;
+            }
+         
+            @Override
+            public void execute(Object args) {
+                if(args instanceof AbstractEnchantmentCardEffect){
+                      AbstractEnchantmentCardEffect enchantment = (AbstractEnchantmentCardEffect) args;
+                    if (enchantment.getOwner() == p) {
+                        choosePermanent(p);
+                      }
+                }
+            }
+           
+        }
+        @Override
+        public void insert() {
+            super.insert();
+            CardGame.instance.getTriggers().register(EFFECT_CASTED, D);
+        }
 
         @Override
-        public String name() {
-            return "Aether Barrier";
+        public void remove() {
+            super.remove();
+            CardGame.instance.getTriggers().deregister(D);
         }
+            @Override
+            public String name() {
+                return "Aether Barrier";
+            }
     }
+        
+   
+    
     @Override
     public Effect getEffect(Player owner) {
         return new AetherBarrierEffect(owner, this);
@@ -58,7 +131,7 @@ public class AetherBarrier implements Card {
 
     @Override
     public String ruleText() {
-        return "Whenever a player plays a creature spell that player sacrifices a permanent unless he or she plays";
+        return "Whenever a player plays a creature spell that player sacrifices a permanent";
     }
 
     @Override
